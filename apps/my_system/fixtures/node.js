be45ceefcf239bd21decd07f7f8ff7d3b674dc15
Richard Klancer <rpk@pobox.com>
2010-07-26 16:49:11 -0400

    Remove links from fixtures.

--- apps/my_system/fixtures/node.js
+++ apps/my_system/fixtures/node.js
@@ -11,15 +11,15 @@ MySystem.Node.FIXTURES = [
   { guid: '1',
     image: '',
     title: 'First Node',
-    inputs: ['2'],
-    outputs: ['2']
+    inputs: [],
+    outputs: []
   },
   
   { guid: '2',
     image: '',
     title: 'Second Node',
-    inputs: ['1'],
-    output: ['1']
+    inputs: [],
+    output: []
   }
   
 ];
