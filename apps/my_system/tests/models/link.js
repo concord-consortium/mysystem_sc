b52fba0fefb0b25b344193065fc83c4b01af7281
Noah Paessel <knowuh@gmail.com>
2010-08-03 12:47:33 -0400

    no tests yet.

---
+++ apps/my_system/tests/models/link.js
@@ -0,0 +1,15 @@
+// ==========================================================================
+// Project:   MySystem.Link Unit Test
+// Copyright: ©2010 My Company, Inc.
+// ==========================================================================
+/*globals MySystem module test ok equals same stop start */
+
+module("MySystem.Link");
+
+// TODO: Replace with real unit test for Link
+test("test description", function() {
+  var expected = "test";
+  var result   = "test";
+  equals(result, expected, "test should equal test");
+});
+
