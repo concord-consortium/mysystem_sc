3a7b7e17a7b546820d7e8f6fb003fbe48a91aa80
Parker Morse <flashesofpanic@gmail.com>
2010-08-11 15:35:40 -0400

    Adds placeholder views for node creation palette and story viewing as SplitViewPanes, with dividers to resize or collapse both.

--- apps/my_system/resources/main_page.js
+++ apps/my_system/resources/main_page.js
@@ -13,13 +13,39 @@ MySystem.mainPage = SC.Page.design({
   // Add childViews to this pane for views to display immediately on page 
   // load.
   mainPane: SC.MainPane.design({
-    childViews: 'systemView'.w(),
-    
-    systemView: LinkIt.CanvasView.design( SCUI.Cleanup, {
-      contentBinding: SC.Binding.from('MySystem.nodesController').oneWay(),
-      selectionBinding: 'MySystem.nodesController.selection',
-      exampleView: MySystem.NodeView
-    })
+	childViews: 'topView'.w(),
+		topView: SC.SplitView.design({
+			defaultThickness: 100,
+			topLeftView: SC.LabelView.design({ // Node Palette (left)
+				layout: { top: 0, bottom: 0, left: 0 },
+				anchorLocation: SC.ANCHOR_LEFT,
+				textAlign: SC.ALIGN_LEFT,
+				fontWeight: SC.BOLD_WEIGHT,
+				backgroundColor: 'Navy',
+				value: "Placeholder for node palette",
+				canCollapse: YES
+			}),
+			dividerView: SC.SplitDividerView, // Divider for resizing right/left
+			bottomRightView: SC.SplitView.design({ // Rest of app (right)
+				defaultThickness: 100,
+				layoutDirection: SC.LAYOUT_VERTICAL,
+				topLeftView: SC.LabelView.design({ // Story section
+					layout: { top: 0, right: 0, left: 0 },
+					anchorLocation: SC.ANCHOR_TOP,
+					textAlign: SC.ALIGN_CENTER,
+					backgroundColor: 'White',
+					value: "Placeholder for story",
+					canCollapse: YES
+				}),
+				dividerView: SC.SplitDividerView, // Divider for resizing up/down
+		    bottomRightView: LinkIt.CanvasView.design( SCUI.Cleanup, { // Workspace
+					layout: { top: 100, left: 0, right: 0, bottom: 0 },
+		      contentBinding: SC.Binding.from('MySystem.nodesController').oneWay(),
+		      selectionBinding: 'MySystem.nodesController.selection',
+		      exampleView: MySystem.NodeView
+		    })
+			})
+		})
   })
 
 });
