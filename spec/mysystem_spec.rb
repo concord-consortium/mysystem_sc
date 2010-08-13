e6b9782657d52f0b31b5d188af86b74ae5b0ce31
Noah Paessel <knowuh@gmail.com>
2010-08-13 17:34:07 -0400

    Simplified Lebowski testing.

--- spec/mysystem_spec.rb
+++ spec/mysystem_spec.rb
@@ -1,97 +1,66 @@
-# require '../../../../lib/lebowski/spec'
-require './support/linkit.rb'
-# require './support/linkit/mixins/link_support.rb'
-#require './support/linkit/mixins/node_item_view_support.rb'
-#require './support/linkit/mixins/terminal_view_support.rb'
-#require './support/linkit/views/linkit.rb'
+require "#{File.dirname(__FILE__)}/support/ms_spec_helper.rb"
 
-require 'node_view'
-
-Spec::Matchers.define :have_node_item_view_support do |sig|
-  match do |obj|
-    obj.respond_to? :has_node_item_view_support
+describe "MySystem" do
+  before(:all) do
+    start_testing_servers
+    @test = new_test
+    @canvas = @test['canvas']
+    @node_0 = @canvas.nodes[0]
+    @node_1 = @canvas.nodes[1]
+    @node_2 = @canvas.nodes[2]
   end
-end
 
-include Lebowski::Foundation
-include Lebowski::SCUI::Views
-include MySystem::Views
-
-ProxyFactory.proxy NodeView
-
-App = MainApplication.new :app_root_path => "/my_system", :app_name => "MySystem", :browser => :firefox
-
-App.start
-App.define 'canvas', 'mainPage.mainPane.topView.bottomRightView.bottomRightView', CanvasView
-
-describe "VIEW: canvas" do
-  describe "TEST: setting up" do
-    before(:all) do
-      @canvas = App['canvas']
-    end
-
-    it "will have at least 3 nodes loaded from the fixtures" do
-      @canvas.should_not be_empty
-      @canvas.nodes.count.should be 3
-    end
+  after(:all) do
+    stop_testing_servers
   end
-    
-  describe "TEST: clicking and dragging" do
-    before(:all) do
-      @canvas = App['canvas']
-      @node_0 = @canvas.nodes[0]
-      @node_1 = @canvas.nodes[1]
-      @node_2 = @canvas.nodes[2]
-    end
 
-    it "will verify that there are 3 nodes on the canvas" do
-      @node_0.should be_a_kind_of NodeView
-      @node_0.should have_node_item_view_support
-    end
-    
-    it "will click each of the nodes, one by one" do
-      @canvas.nodes.each do |node|
-        node.click
-        node.should be_selected
-      end
-    end
-    
-    it "will drag the first node to coordinates (400, 150)" do
-      @node_0.drag_in_canvas(400, 150)
-    end
-    
-    it "will drag the second node above the first node (by the index)" do 
-      @node_1.drag_above 0
-      @node_1.should be_positioned_above 0
-    end
+  it "will have at least 3 nodes loaded from the fixtures" do
+    @canvas.should_not be_empty
+    @canvas.nodes.count.should be 3
   end
-  
-  describe "TEST: linking" do
-    before(:all) do
-      @canvas = App['canvas']
-      @node_0 = @canvas.nodes[0]
-      @node_1 = @canvas.nodes[1]
-      @node_2 = @canvas.nodes[2]      
-    end
-    
-    # tricky & brittle! relies on fixture state
-    # as of 
-    it "some nodes loaded from fixtures should be linked" do
-      @node_0.should be_linked_to 1
-      @node_1.should be_linked_to 0
       
-      @node_0.should be_linked_to 2
-      @node_2.should be_linked_to 0
-
-      @node_1.should_not be_linked_to 2
-      @node_2.should_not be_linked_to 1
-    end
-  
-    it "will link node_1 to node_2" do      
-      @node_2.terminal_by_name('b').link_to @node_1.terminal_by_name('b'), 3, 3
-      wait 20
-      @node_2.should be_linked_to 1
-    end
+  it "will verify that there are 3 nodes on the canvas" do
+    @node_0.should be_a_kind_of NodeView
+    @node_0.should have_node_item_view_support
   end
   
+  it "will click each of the nodes, one by one" do
+    @canvas.nodes.each do |node|
+      node.click
+      node.should be_selected
+    end
+  end
+
+  it "will drag the first node to coordinates (400, 150)" do
+    @node_0.drag_in_canvas(400, 150)
+  end
+  
+  it "will drag the second node below the first node (by the index)" do 
+    @node_1.drag_below 0
+    @node_1.should be_positioned_below 0
+  end
+
+  it "will drag the third node to the right of the second node (by index)" do
+    @node_2.drag_right_of 1
+    @node_2.should be_positioned_right_of 1
+  end
+
+  # tricky & brittle! relies on fixture state
+  # as of 
+  it "some nodes loaded from fixtures should be linked" do
+    @node_0.should be_linked_to 1
+    @node_1.should be_linked_to 0
+    
+    @node_0.should be_linked_to 2
+    @node_2.should be_linked_to 0
+
+    @node_1.should_not be_linked_to 2
+    @node_2.should_not be_linked_to 1
+  end
+
+  it "will link node_1 to node_2" do      
+    @node_2.terminal_by_name('a').link_to @node_1.terminal_by_name('a'), 8, 8 
+    @node_2.should be_linked_to 1
+  end
 end
+
---
+++ spec/support/ms_spec_helper.rb
@@ -0,0 +1,105 @@
+dir = File.dirname(__FILE__) 
+Dir.glob(dir + '/linkit/**/*.rb') {|file| require file}
+require "#{dir}/node_view.rb"
+
+include Lebowski::Foundation
+include Lebowski::SCUI::Views
+include MySystem::Views
+
+
+ProxyFactory.proxy NodeView
+
+TEST_PORT =  ENV[:TEST_PORT] || 4022;
+TEST_SETTINGS = {
+  :app_root_path => "/my_system",
+  :app_name => "MySystem",
+  :app_server_port => TEST_PORT,
+  :browser => :firefox
+}
+
+
+$commands = {
+  :sproutcore => {
+    :path => "sc-server --port #{TEST_PORT}",
+    :name => "sproutcore server",
+    :pid => nil
+  },
+  :lebowski => {
+    :path => 'lebowski-start-server',
+    :name => "lebowski",
+    :pid => nil
+  }
+}
+
+Spec::Matchers.define :have_node_item_view_support do |sig|
+  match do |obj|
+    obj.respond_to? :has_node_item_view_support
+  end
+end
+
+
+# create a new started test applicaion 
+# configured with mysystem settings
+
+def new_test
+  app =  MainApplication.new TEST_SETTINGS
+  app.start
+  app.maximize  # TODO: Seems like dragging doesn't work unless we are maximized.
+  sleep 1       # TODO: hackish pause, CanvasView is not ready otherwise..
+  app.define 'canvas', 'mainPage.mainPane.topView.bottomRightView.bottomRightView', CanvasView
+  return app
+end
+
+def start_command(name)
+  command = $commands[name.to_sym]
+  unless command[:pid]
+    command[:pid] = fork do
+      puts "Starting process  #{command[:name] || name} with #{command[:path]} #{command[:args]}"
+      Signal.trap("HUP") do
+        puts "Stopping process #{command[:name] || name}"
+        exit
+      end
+      if (command[:args])
+        exec(command[:path] || name, command[:args])
+      else
+        exec(command[:path])
+      end
+    end
+    puts "Started  #{command[:name] || name} with PID: #{command[:pid]}" 
+  else
+    puts "WARNL: process  #{command[:name] || name} already started with #{command[:pid]}"
+  end
+  sleep 1 # Hackish pause to spin up job.
+end
+
+
+def stop_command(name)
+  command = $commands[name.to_sym]
+  if command && command[:pid]
+    Process.kill('TERM',command[:pid])
+    Process.wait(command[:pid])
+    command[:pid] = nil;
+    puts "#{command[:name] || name} stopped"
+  else
+    puts "WARNING: #{command[:name] || name} does not seem to be running"
+  end
+end
+
+def start_testing_servers
+  $commands.keys.each do |command|
+    start_command(command)
+  end
+end
+
+def stop_testing_servers
+  $commands.keys.each do |command|
+    stop_command(command)
+  end
+end
+
+def with_lebowsk_server (&block)
+  start_testing_servers
+  sleep 2 #shouldn't have to wait, but there ya-go.
+  yield
+  stop_testing_servers
+end
