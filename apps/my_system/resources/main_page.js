e797c6cddab8fd151383e9b5cb9f402d51c0ef3f
Parker Morse <parker.morse@commonmediainc.com>
2010-08-16 11:49:04 -0400

    Whitespace cleanup, commenting extra buttons until I can get one right.

--- apps/my_system/resources/main_page.js
+++ apps/my_system/resources/main_page.js
@@ -13,65 +13,68 @@ MySystem.mainPage = SC.Page.design({
   // Add childViews to this pane for views to display immediately on page 
   // load.
   mainPane: SC.MainPane.design({
-	childViews: 'topView'.w(),
-		topView: SC.SplitView.design({
-			defaultThickness: 120,
-			topLeftView: SC.View.design({ // Node Palette (left)
-				layout: { top: 0, bottom: 0, left: 0 },
-	      childViews: 'addDecorator addClay addHand addBulb'.w(),
+  childViews: 'topView'.w(),
+    topView: SC.SplitView.design({
+      defaultThickness: 120,
+      topLeftView: SC.View.design({ // Node Palette (left)
+        layout: { top: 0, bottom: 0, left: 15 },
+        childViews: 'addDecorator addClay'.w(),
 
-	      addDecorator: SC.View.design({
-	        layout: { left: 0, right: 0, top: 0, height: 23 },
-	        classNames: ['add-decorator']
-	      }),
+        addDecorator: SC.View.design({
+          layout: { left: 20, right: 0, top: 0, height: 23, width: 80 },
+          classNames: ['add-decorator']
+        }),
 
-	      addClay: MySystem.AddButtonView.design({
-	        layout: { left: 0, right: 0, top: 23, height: 65 },
-	        classNames: ['add-clay'],
-	        title: "Clay",
-	        target: MySystem.nodesController,
-	        action: 'addClay'
-	      }),
+        addClay: MySystem.AddButtonView.design({
+          layout: { left: 20, right: 0, top: 23, width: 100, height: 120 },
+          classNames: ['add-clay'],
+          title: "Clay",
+          icon: 'http://ccmysystem.appspot.com/images/At-Concord-Fall/clay_red_tn.png',
+          target: MySystem.nodesController,
+          action: 'addClay'
+        })
 
-	      addHand: MySystem.AddButtonView.design({
-	        layout: { left: 0, right: 0, top: 88, height: 66 },
-	        classNames: ['add-hand'],
-	        title: "Hand",
-	        target: MySystem.nodesController,
-	        action: 'addHand'
-	      }),
-
-	      addBulb: MySystem.AddButtonView.design({
-	        layout: { left: 0, right: 0, top: 154, height: 68 },
-	        classNames: ['add-bulb'],
-	        title: "Bulb",
-	        target: MySystem.nodesController,
-	        action: 'addBulb'
-	      })
-			}),
-			dividerView: SC.SplitDividerView, // Divider for resizing right/left
-			bottomRightView: SC.SplitView.design({ // Rest of app (right)
-				defaultThickness: 120,
-				layoutDirection: SC.LAYOUT_VERTICAL,
-				topLeftView: SC.LabelView.design({ // Story section
-					layout: { top: 0, right: 0, left: 0 },
-					anchorLocation: SC.ANCHOR_TOP,
-					textAlign: SC.ALIGN_LEFT,
-					backgroundColor: '#CCCCFF',
-					tagName: "div",
-					escapeHTML: NO,
-					valueBinding: 'MySystem.storyController.content.storyHtml',
-					canCollapse: YES
-				}),
-				dividerView: SC.SplitDividerView, // Divider for resizing up/down
-		    bottomRightView: LinkIt.CanvasView.design( SCUI.Cleanup, { // Workspace
-					layout: { top: 120, left: 0, right: 0, bottom: 0 },
-		      contentBinding: SC.Binding.from('MySystem.nodesController').oneWay(),
-		      selectionBinding: 'MySystem.nodesController.selection',
-		      exampleView: MySystem.NodeView
-		    })
-			})
-		})
+        // addHand: MySystem.AddButtonView.design({
+        //   layout: { left: 20, right: 0, top: 153, width: 100, height: 120 },
+        //   classNames: ['add-hand'],
+        //   icon: 'http://ccmysystem.appspot.com/images/At-Concord-Fall/hand_tn.png',
+        //   title: "Hand",
+        //   target: MySystem.nodesController,
+        //   action: 'addHand'
+        // }),
+        // 
+        // addBulb: MySystem.AddButtonView.design({
+        //   layout: { left: 20, right: 0, top: 283, width: 100, height: 120 },
+        //   classNames: ['add-bulb'],
+        //   icon: 'http://ccmysystem.appspot.com/images/At-Concord-Fall/lightbulb_tn.png',
+        //   title: "Bulb",
+        //   target: MySystem.nodesController,
+        //   action: 'addBulb'
+        // })
+      }),
+      dividerView: SC.SplitDividerView, // Divider for resizing right/left
+      bottomRightView: SC.SplitView.design({ // Rest of app (right)
+        defaultThickness: 120,
+        layoutDirection: SC.LAYOUT_VERTICAL,
+        topLeftView: SC.LabelView.design({ // Story section
+          layout: { top: 0, right: 0, left: 0 },
+          anchorLocation: SC.ANCHOR_TOP,
+          textAlign: SC.ALIGN_LEFT,
+          backgroundColor: '#CCCCFF',
+          tagName: "div",
+          escapeHTML: NO,
+          valueBinding: 'MySystem.storyController.content.storyHtml',
+          canCollapse: YES
+        }),
+        dividerView: SC.SplitDividerView, // Divider for resizing up/down
+        bottomRightView: LinkIt.CanvasView.design( SCUI.Cleanup, { // Workspace
+          layout: { top: 120, left: 0, right: 0, bottom: 0 },
+          contentBinding: SC.Binding.from('MySystem.nodesController').oneWay(),
+          selectionBinding: 'MySystem.nodesController.selection',
+          exampleView: MySystem.NodeView
+        })
+      })
+    })
   })
 
 });
