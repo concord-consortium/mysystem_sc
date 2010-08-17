ed11a5b9fd6debd9e1cad10dc7b151cafee5571c
Noah Paessel <knowuh@gmail.com>
2010-08-17 13:36:17 -0400

    capy tests are now configured in hudson config

--- build.sh
+++ build.sh
@@ -37,11 +37,8 @@ git submodule update
 # export CI_REPORTS=reports
 # export CI_FORMATTER=/home/maven/.rvm/gems/${RVM_RUBY}/gems/ci_reporter-1.6.2/lib/ci/reporter/rake/rspec_loader
 # spec --require ${CI_FORMATTER} --format CI::Reporter::RSpec ./spec/mysystem_spec.rb
-
-echo "*** Running CapyBara TestRunner tests ***"
-pushd capybara-testrunner
-ruby run-tests.rb -s 0.0.0.0 -p 4020 -t apps -o ../reports
-popd
+# > 
+# capy tests are now configured in hudson config
 
 echo
 echo "********************************************"
