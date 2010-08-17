97fe0a927914e90aa827756922ccd31d6fcbe980
Noah Paessel <knowuh@gmail.com>
2010-08-17 18:27:28 -0400

    Extract MySystem.CanvasView

---
+++ apps/my_system/tests/views/canvas.js
@@ -0,0 +1,15 @@
+// ==========================================================================
+// Project:   MySystem.CanvasView Unit Test
+// Copyright: ©2010 My Company, Inc.
+// ==========================================================================
+/*globals MySystem module test ok equals same stop start */
+
+module("MySystem.CanvasView");
+
+// TODO: Replace with real unit test for MySystem.CanvasView
+test("test description", function() {
+  var expected = "test";
+  var result   = "test";
+  equals(result, expected, "test should equal test");
+});
+
---
+++ apps/my_system/views/canvas.js
@@ -0,0 +1,29 @@
+// ==========================================================================
+// Project:   MySystem.CanvasView
+// Copyright: ©2010 My Company, Inc.
+// ==========================================================================
+/*globals MySystem LinkIt SCUI SC*/
+
+/** @class
+
+  (Document Your View Here)
+
+  @extends SC.View
+*/
+sc_require('MySystem.NodeView');
+sc_require('LinkIt.CanvasView');
+sc_require('core');
+MySystem.CanvasView = LinkIt.CanvasView.extend(SCUI.Cleanup, SC.DropTarget, { 
+    isDropTarget: YES,
+    computeDragOperations: function(drag, evt) { return SC.DRAG_COPY; },
+    performDragOperation: function(drag, op) { 
+      var image = drag.data.image;
+      var title = drag.data.title;
+      var xpos = drag.location.x;
+      var ypos = drag.location.y;
+      //( title, image, xPos, yPos )
+      SC.Logger.log("drag performed");
+      MySystem.nodesController.addNode(image,title,xpos,ypos);
+      return SC.DRAG_COPY; 
+    }
+});
