ebb3745ff2710b5048968ef8d925b64dc94616ba
Parker Morse <parker.morse@commonmediainc.com>
2010-08-17 10:22:08 -0400

    Controller function to place new node (randomly) on the canvas.

--- apps/my_system/controllers/nodes.js
+++ apps/my_system/controllers/nodes.js
@@ -12,14 +12,23 @@
 */
 MySystem.nodesController = SC.ArrayController.create( SC.CollectionViewDelegate, 
 /** @scope MySystem.nodesController.prototype */ {
-  
+
   collectionViewDeleteContent: function (view, content, indices) {
     // destroy the records
     var recordsToDestroy = indices.map( function (idx) {
       return this.objectAt(idx);
     }, this);
-    
+
     recordsToDestroy.invoke('destroy');
+  },
+
+  addNode: function () {
+    var node;
+
+    // Create a new node in store
+    node = MySystem.store.createRecord(MySystem.Node, { "title": "Edit Title", "image": 'http://ccmysystem.appspot.com/images/At-Concord-Fall/lightbulb_tn.png' } );
+
+    return YES;
   }
 
 }) ;
