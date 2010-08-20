562c9aaf031443ba73c0823a537f805cf5696d84
John Ridgway <john@haskell.local>
2010-08-20 15:26:34 -0400

    Fix drag and drop 4765087

--- apps/my_system/views/canvas.js
+++ apps/my_system/views/canvas.js
@@ -23,9 +23,11 @@ MySystem.CanvasView = LinkIt.CanvasView.extend(SCUI.Cleanup, SC.DropTarget, {
       var ypos = drag.location.y;
       var offsetX = this.parentView.get('frame').x;
       var offsetY = this.get('frame').y;
+			var gX = drag.ghostOffset.x;
+			var gY = drag.ghostOffset.y;
       //( title, image, xPos, yPos )
       SC.Logger.log("drag performed");
-      MySystem.nodesController.addNode(title, image, xpos - offsetX, ypos - offsetY);
+      MySystem.nodesController.addNode(title, image, xpos - offsetX - gX, ypos - offsetY - gY);
       return SC.DRAG_COPY; 
     }
 });
