5ebb1fed9f78e53e018277432ef2f772e884a250
John Ridgway <john.ridgway@commonmmediainc.com>
2010-08-27 12:07:51 -0400

    Vanishing property editor panel

---
+++ apps/my_system/views/property_editor.js
@@ -0,0 +1,65 @@
+sc_require('core');
+
+MySystem.PropertyEditorPane = SC.Pane.extend(
+{
+    layout: {
+        top: 0,
+        left: 0,
+        width: 100,
+        height: 120
+    },
+    classNames: 'node'.w(),
+
+    displayProperties: 'content isSelected'.w(),
+    content: null,
+    isSelected: false,
+
+    childViews: 'label'.w(),
+
+    render: function(context) {
+        sc_super();
+        if (this.get('isSelected')) context.addClass('selected');
+    },
+
+    // icon: SC.ImageView.design({
+    //     classNames: 'image',
+    //     useImageCache: true,
+    //     layout: {
+    //         top: 20,
+    //         width: 50,
+    //         height: 70,
+    //         centerX: 0
+    //     },
+    //     valueBinding: '.parentView.image'
+    // }),
+
+    label: SC.LabelView.design({
+        layout: {
+            bottom: 5,
+            centerX: 0,
+            width: 100,
+            height: 25
+        },
+        classNames: ['name'],
+        textAlign: SC.ALIGN_CENTER,
+        value: '.parentView.title',
+        isEditable: NO
+    }),
+
+    mouseDown: function(eventID) {
+        SC.Logger.log("mouseDown called");
+        var self = this;
+        var dragOpts = {
+            event: eventID,
+            source: self.get('parentView'),
+            dragView: self,
+            ghost: NO,
+            slideBack: NO,
+            data: {
+                title: this.get('title') || 'title',
+                image: this.get('image') || 'image'
+            }
+        };
+        SC.Drag.start(dragOpts);
+    }
+});
