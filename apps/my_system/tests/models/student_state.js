0a537aca33b78fba14d898d9062cdf0ad79c278e
Parker Morse <flashesofpanic@gmail.com>
2010-08-31 11:59:54 -0400

    QUnit testing the StudentState model, particularly returning editable form fields for Property Editor (it responds but returns no fields)

--- apps/my_system/tests/models/student_state.js
+++ apps/my_system/tests/models/student_state.js
@@ -8,6 +8,10 @@ module("MySystem.StudentState");
 
 sc_require('resources/student_data_example.js');
 
+function isArray(testObject) {
+  return testObject && !testObject.propertyIsEnumerable('length') && typeof testObject === 'object' && typeof testObject.length === 'number';
+}
+
 test("Testing model definition of Student State", function() {
   expect(3);
   // Imports the same data as the fixture
@@ -16,5 +20,13 @@ test("Testing model definition of Student State", function() {
   equals(typeof state.content, "string", "Content should be a string");
   equals(typeof state.timestamp, "number", "Timestamp should be a number");
   equals(timestamp.getDate(), 12, "Date should be the 12th");
+});
+
+test("Student state should return an array of editable form fields when asked", function() {
+  expect(2);
+  var state = MySystem.store.find('MySystem.StudentState', 1);
+  var formFields = state.get('formFields');
+  ok(isArray(formFields), "The formFields attribute should return an array");
+  equals(formFields.length, 0, "StudentState should not return any editable fields");
 });
 
