f3596c8c50670fe65143a9f7972ccad38cb085af
Parker Morse <flashesofpanic@gmail.com>
2010-08-31 18:07:41 -0400

    Updating the node view QUnit test to be more detailed.

--- apps/my_system/tests/views/node.js
+++ apps/my_system/tests/views/node.js
@@ -2,16 +2,20 @@
 // Project:   MySystem.NodeView Unit Test
 // Copyright: ©2010 My Company, Inc.
 // ==========================================================================
-/*globals MySystem module test ok equals same stop start */
+/*globals MySystem LinkIt SC module test ok equals same stop start */
 
 module("MySystem.NodeView");
 
 var node = MySystem.NodeView.create();
 
 test("Node was allocated.", function() {
-  expect(3);
+  expect(7);
   ok(node !== null, "node is not null.");
   ok(node.kindOf(MySystem.NodeView), "node is actually a NodeView");
   ok(node.classNames.indexOf('node') != -1, "node has appropriate class.");
+  equals(node.get('childViews').length, 4, "addButton should have four children.");
+  ok(node.get('icon').kindOf(SC.ImageView), "icon should be a SproutCore ImageView");
+  ok(node.get('label').kindOf(SC.LabelView), "label should be a SproutCore LabelView");
+  ok(node.get('aTerminal').kindOf(SC.View), "Terminals should be SproutCore Views");
 });
 
