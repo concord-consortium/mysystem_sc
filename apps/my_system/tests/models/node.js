f24e1bd7ff5c9cca442b6f2bf09764b32ffe0564
Parker Morse <flashesofpanic@gmail.com>
2010-08-31 11:55:41 -0400

    Adding QUnit tests for the Node model: returning form field editable attributes array and new-Guid creation

--- apps/my_system/tests/models/node.js
+++ apps/my_system/tests/models/node.js
@@ -6,6 +6,10 @@
 
 module("MySystem.Node");
 
+function isArray(testObject) {
+  return testObject && !testObject.propertyIsEnumerable('length') && typeof testObject === 'object' && typeof testObject.length === 'number';
+}
+
 test("test title from fixture", function() {
   expect(1);
   var node   = MySystem.store.find('MySystem.Node','1');
@@ -58,9 +62,26 @@ test("test that computed 'links' are updated when inlinks or outlinks changes", 
   equals(foundLinks, expectedLinks, "There should be "+ expectedLinks +" links");
 });
 
-test ("nodes should have a position attribute", function() {
+test("nodes should have a position attribute", function() {
   expect(2);
   var node = MySystem.store.createRecord(MySystem.Node, {'title': 'Edit Title', 'image': 'http://concord.org/favicon.ico', 'position': {'x': 120, 'y': 150}, 'guid': 'testNode001'});
   equals(node.get('position').x, 120, "X position should be 120 as created");
   equals(node.get('position').y, 150, "Y position should be 150 as created");
+});
+
+test("nodes should return an array of editable form fields when asked", function() {
+  expect(2);
+  var ccNode = MySystem.store.find('MySystem.Node', 'testNode001');
+  var formFields = ccNode.get('formFields');
+  ok(isArray(formFields), "The formFields attribute should return an array");
+  equals(formFields.length, 2, "Nodes should return two editable fields");
+});
+
+test("We should be able to generate a new GUID", function() {
+  expect(3);
+  var newGuid = MySystem.Node.newGuid();
+  var newGuid2 = MySystem.Node.newGuid();
+  ok(MySystem.Node.newGuid(), "The newGuid function should return a defined value");
+  equals(typeof newGuid, 'string', "The newGuid function should return strings");
+  ok((newGuid2 != newGuid), 'The second newGuid should not be the same as the first');
 });
