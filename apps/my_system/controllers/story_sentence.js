2da9f7bc55565e8a427f3640d3b6d21d1ef9a873
Parker Morse <flashesofpanic@gmail.com>
2010-09-14 20:04:32 -0400

    Story sentences can be added and deleted.
    But not edited, frustratingly.

--- apps/my_system/controllers/story_sentence.js
+++ apps/my_system/controllers/story_sentence.js
@@ -11,8 +11,41 @@
   @extends SC.ArrayController
 */
 MySystem.storySentenceController = SC.ArrayController.create(
+  SC.CollectionViewDelegate,
 /** @scope MySystem.storySentencesController.prototype */ {
 
-  // TODO: Add your own code here.
+  collectionViewDeleteContent: function(view, content, indexes) {
+    // Destroy the records
+    var records = indexes.map(function(idx) {
+      return this.objectAt(idx);
+    }, this);
+    records.invoke('destroy');
 
+    var selIndex = indexes.get('min')-1;
+    if (selIndex<0) selIndex = 0;
+    this.selectObject(this.objectAt(selIndex));
+  },
+
+  addStorySentence: function() {
+    var sentence;
+
+    // Create a new sentence in the store
+    sentence = MySystem.store.createRecord(MySystem.StorySentence, {
+      "guid": MySystem.StorySentence.newGuid(),
+      "bodyText": "Describe a new step in the story."
+    });
+
+    // Select the sentence in the UI
+    this.selectObject(sentence);
+
+    // Activate the editor once the UI repaints
+    this.invokeLater(function() {
+      var contentIndex = this.indexOf(sentence);
+      var list = MySystem.mainPage.getPath('mainPane.topView.bottomRightView.topLeftView.bottomRightView.sentencesView.contentView');
+      var listItem = list.itemViewForContentIndex(contentIndex);
+      listItem.beginEditing();
+    });
+
+    return YES;
+  }
 }) ;
--- apps/my_system/resources/main_page.js
+++ apps/my_system/resources/main_page.js
@@ -67,17 +67,31 @@ MySystem.mainPage = SC.Page.design({
             canCollapse: YES
           }),
           dividerView: SC.SplitDividerView, // Divider for resizing up/down
-          bottomRightView: SC.ScrollView.design({
-            hasHorizontalScroller: NO,
-            layout: { top: 36, bottom: 32, left: 0, right: 0 },
-            backgroundColor: 'white',
-            contentView: SC.ListView.design({
-              contentBinding: 'MySystem.storySentenceController.arrangedObjects',
-              selectionBinding: 'MySystem.storySentenceController.selection',
-              contentValueKey: "bodyText",
-              rowHeight: 20,
-              canEditContent: YES,
-              canDeleteContent: YES
+          bottomRightView: SC.View.design({
+            childViews: 'toolbar sentencesView'.w(),
+            toolbar: SC.ToolbarView.design({
+              layout: { top: 0, left: 0, right: 0, height: 30 },
+              childViews: 'addButton'.w(),
+              anchorLocation: SC.ANCHOR_TOP,
+              addButton: SC.ButtonView.design({
+                layout: { centerY: 0, height: 20, width: 150 },
+                title: "Add sentence to story",
+                target: "MySystem.storySentenceController",
+                action: "addStorySentence"
+              })
+            }),
+            sentencesView: SC.ScrollView.design({
+              hasHorizontalScroller: NO,
+              layout: { top: 30, bottom: 0, left: 0, right: 0 },
+              backgroundColor: 'white',
+              contentView: SC.ListView.design({
+                contentBinding: 'MySystem.storySentenceController.arrangedObjects',
+                selectionBinding: 'MySystem.storySentenceController.selection',
+                contentValueKey: "bodyText",
+                rowHeight: 20,
+                canEditContent: YES, // isEditable: YES, // FIXME: Neither of these are working
+                canDeleteContent: YES
+              })
             })
           })
         }),
