1ddd406d7fe34fb1db4ba2de81d9d03de7fc9115
Parker Morse <flashesofpanic@gmail.com>
2010-09-23 17:43:21 -0400

    Sentence-to-diagram-linking buttons have toggle behavior.

--- apps/my_system/views/sentence.js
+++ apps/my_system/views/sentence.js
@@ -24,6 +24,7 @@ MySystem.SentenceView = SC.View.extend(
   linkButton: SC.ButtonView.design({
     layout: { right: 5, width: 40 },
     titleMinWidth: 45,
+    buttonBehavior: SC.TOGGLE_BEHAVIOR,
     icon: sc_static('resources/icon_link.gif'),
     toolTip: "Link this sentence with part of the diagram"
   })
