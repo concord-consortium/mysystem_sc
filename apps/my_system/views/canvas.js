553f67b5c575f677d551e01d49f73bebd28bf9a1
John Ridgway <john.ridgway@commonmmediainc.com>
2010-09-28 09:39:51 -0400

    Changes for SproutCore 1.4

--- apps/my_system/views/canvas.js
+++ apps/my_system/views/canvas.js
@@ -30,5 +30,31 @@ MySystem.CanvasView = LinkIt.CanvasView.extend(SCUI.Cleanup, {
       MySystem.nodesController.addNode(title, image, xpos - offsetX - gX, ypos - offsetY - gY);
       MySystem.nodesController.deselectObjects(MySystem.nodesController.get('allSelected'));
       return SC.DRAG_COPY; 
-    }
+    },
+		didCreateLayer: function () {
+			var frame = this.get('frame');
+      var canvasElem = this.$('canvas.base-layer');
+      if (canvasElem) {
+        canvasElem.attr('width', frame.width);
+        canvasElem.attr('height', frame.height);
+        if (canvasElem.length > 0) {
+          var cntx = canvasElem[0].getContext('2d'); // Get the actual canvas object context
+          if (cntx) {
+            cntx.clearRect(0, 0, frame.width, frame.height);
+            this._drawLinks(cntx);
+          }
+          else {
+            LinkIt.log("Linkit.LayerView.render(): Canvas object context is not accessible.");
+          }
+        }
+        else {
+          LinkIt.log("Linkit.LayerView.render(): Canvas element array length is zero.");
+        }
+      }
+      else {
+        LinkIt.log("Linkit.LayerView.render(): Canvas element is not accessible.");
+      }
+    
+    return sc_super();
+  }
 });
