9bae509898efe225b56422d0b97008a657958516
Parker Morse <flashesofpanic@gmail.com>
2010-09-29 10:21:18 -0400

    Whitespace and formatting cleanup on sentence connect pane

--- apps/my_system/views/sentence_connect_pane.js
+++ apps/my_system/views/sentence_connect_pane.js
@@ -15,7 +15,7 @@
 */
 MySystem.SentenceConnectPane = SC.PalettePane.extend(
 /** @scope MySystem.SentenceConnectPane.prototype */ {
-	isEnabled: YES,
+  isEnabled: YES,
   acceptsFirstResponder: YES,
   acceptsKeyPane: YES,
   layout: { top: 150, right: 5, width: 150, height: 150 },
@@ -24,42 +24,36 @@ MySystem.SentenceConnectPane = SC.PalettePane.extend(
   activeSentence: null,
   // The selected objects in the diagram
   selectedObjectsBinding: 'MySystem.nodesController.allSelected',
-	contentView: SC.View.design({
-	// 	acceptsFirstResponder: YES,
-	// 	acceptsKeyPane: YES,
-	// 	isEnabled: YES,
-	 	childViews: "labelView doneButton".w(),
-	  	labelView: SC.LabelView.create({
-	  		    	layout: { left: 5, right: 50, left: 0, width: 50 },
-	  		    	// valueBinding: '.parentView*content.bodyText',
-	  					value: 'XYZ',
-	  		    	canEditContent: YES,
-	  		    	canDeleteContent: YES,
-	  		    	isEditable: YES,
-			mouseDown: function(evt) {console.log("label mouse down"); return YES;}
-	  	}),
-	  	doneButton: SC.ButtonView.create({
-			buttonBehavior: SC.PUSH_BEHAVIOR,
-			layout: { left: 5, right: 55, top: 30, height:20 },
-	    icon: sc_static('resources/icon_link.gif'),
-			title: "Done",
-	    toolTip: "Click to link and close this window",
-			target: MySystem.storyController,
-			action: "doneButtonPushed",
-			theme: "capsule",
-			value: NO,
-			isEnabled: YES,
-			mouseDown: function(evt) {console.log("button mouse down"); return YES;}
-		}),
-		doneButtonPushed: function() {
-			debugger;
-		},
-		mouseDown: function(evt) { console.log("mouse down"); return YES;},
-		mouseUp: function(evt) { console.log("mouse up"); return NO;}
-		}),
-	doneButtonPushed: function() {
-		debugger;
-	},
-	mouseDown: function(evt) { console.log("mouse down"); return YES;},
-	mouseUp: function(evt) { console.log("mouse up"); return NO;}
+  contentView: SC.View.design({
+    //  acceptsFirstResponder: YES,
+    //  acceptsKeyPane: YES,
+    //  isEnabled: YES,
+    childViews: "labelView doneButton".w(),
+    labelView: SC.LabelView.create({
+      layout: { left: 5, right: 5, top: 5, width: 140, height: 80 },
+      value: "Select the nodes and links your sentence describes, then click the 'Done' button.",
+      canEditContent: YES,
+      canDeleteContent: YES,
+      isEditable: YES,
+      mouseDown: function(evt) {console.log("label mouse down"); return YES;}
+    }),
+    doneButton: SC.ButtonView.create({
+      acceptsFirstResponder: YES,
+      buttonBehavior: SC.PUSH_BEHAVIOR,
+      layout: { left: 10, right: 10, bottom: 10, height:20 },
+      // icon: sc_static('resources/icon_link.gif'),
+      title: "Done",
+      toolTip: "Click to link and close this window",
+      target: MySystem.storyController,
+      action: "doneButtonPushed",
+      theme: "capsule",
+      value: NO,
+      isEnabled: YES
+      // mouseDown: function(evt) { console.log("mouse down in button"); return YES; },
+      // mouseUp: function(evt) { console.log("mouse up in button"); return NO; }
+    }),
+    doneButtonPushed: function() { console.log("Done button pushed in contentView"); return YES; },
+    mouseDown: function(evt) { console.log("mouse down in contentView"); return YES; },
+    mouseUp: function(evt) { console.log("mouse up in contentView"); return NO; }
+  })
 });
