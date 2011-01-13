4bb99afa80f8678881ed44fa1897d3e9c182d955
Parker Morse <flashesofpanic@gmail.com>
2011-01-13 13:03:49 -0500

    Fixing unit tests broken by removal of Transformation code

--- apps/my_system/tests/views/node.js
+++ apps/my_system/tests/views/node.js
@@ -9,13 +9,15 @@ module("MySystem.NodeView");
 var node = MySystem.NodeView.create();
 
 test("Node was allocated.", function() {
-  expect(8);
+  expect(7);
   ok(node !== null, "node is not null.");
   ok(node.kindOf(MySystem.NodeView), "node is actually a NodeView");
   ok(node.classNames.indexOf('node') != -1, "node has appropriate class.");
-  equals(node.get('childViews').length, 5, "addButton should have five children.");
+  /* Temporarily removed for Berkeley 0.1 release */
+  // equals(node.get('childViews').length, 5, "addButton should have five children.");
+  equals(node.get('childViews').length, 4, "addButton should have four children.");
   ok(node.get('icon').kindOf(SC.ImageView), "icon should be a SproutCore ImageView");
-  ok(node.get('transformationIcon').kindOf(SC.ImageView), "Transformation Icon should be a SproutCore ImageView");
+  // ok(node.get('transformationIcon').kindOf(SC.ImageView), "Transformation Icon should be a SproutCore ImageView");
   ok(node.get('label').kindOf(SC.LabelView), "label should be a SproutCore LabelView");
   ok(node.get('aTerminal').kindOf(SC.View), "Terminals should be SproutCore Views");
 });
