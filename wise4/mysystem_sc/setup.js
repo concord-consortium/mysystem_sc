52b429c354e3790c4063acad8ba7f3226f3daafe
Noah Paessel <knowuh@gmail.com>
2011-01-24 20:01:58 -0500

    whitespace cleanup.

--- wise4/mysystem_sc/setup.js
+++ wise4/mysystem_sc/setup.js
@@ -1,40 +1,41 @@
 var coreScripts = [
-	'vle/node/mysystem/MySystemNode.js',
-	'vle/node/mysystem/mySystemEvents.js',
-	/* 
+  'vle/node/mysystem_sc/MySystemNode.js',
+  'vle/node/mysystem_sc/mySystemEvents.js',
+  /* 
      * the following are needed here for showallwork 
      */
-	'vle/node/mysystem/mysystem_complete.js',
-    'vle/node/mysystem/mysystem_print.js'
+  'vle/node/mysystem_sc/mysystem_complete.js',
+  'vle/node/mysystem_sc/mysystem_print.js'
+  //TODO use some template engine, and recurse through files.
 ];
 
 var studentVLEScripts = [
-	'vle/node/mysystem/mysystem_complete.js',
-	'vle/node/mysystem/mysystem_print.js',
-	'vle/jquery/js/jquery-1.4.2.min.js',
-	'vle/jquery/js/jquery-ui-1.8.custom.min.js',
-	'vle/jquery/js/jsonplugin.js'
+  'vle/node/mysystem_sc/mysystem_complete.js',
+  'vle/node/mysystem_sc/mysystem_print.js',
+  'vle/jquery/js/jquery-1.4.2.min.js',
+  'vle/jquery/js/jquery-ui-1.8.custom.min.js',
+  'vle/jquery/js/jsonplugin.js'
 ];
 
 var authorScripts = [
-	'vle/node/mysystem/authorview_mysystem.js'
+  'vle/node/mysystem_sc/authorview_mysystem.js'
 ];
 
 var gradingScripts = [
-	'vle/node/mysystem/mysystemstate.js'
+  'vle/node/mysystem_sc/mysystemstate.js'
 ];
 
 var dependencies = [
-	{child:"vle/node/mysystem/MySystemNode.js", parent:["vle/node/Node.js"]}
+  {child:"vle/node/mysystem_sc/MySystemNode.js", parent:["vle/node/Node.js"]}
 ];
 
 var css = [
-	'vle/node/mysystem/css/YUI/reset-font-grids.css',
-	'vle/node/mysystem/css/YUI/base-min.css',
-	'vle/node/mysystem/css/YUI/sam.css',
-	'vle/node/mysystem/css/WireIt.css',
-	'vle/node/mysystem/css/WireItEditor.css',
-	'vle/node/mysystem/css/mysystem.css'
+  'vle/node/mysystem_sc/css/YUI/reset-font-grids.css',
+  'vle/node/mysystem_sc/css/YUI/base-min.css',
+  'vle/node/mysystem_sc/css/YUI/sam.css',
+  'vle/node/mysystem_sc/css/WireIt.css',
+  'vle/node/mysystem_sc/css/WireItEditor.css',
+  'vle/node/mysystem_sc/css/mysystem.css'
 ];
 
 scriptloader.addScriptToComponent('core', coreScripts);
@@ -46,5 +47,5 @@ scriptloader.addCssToComponent('mysystem', css);
 
 //used to notify scriptloader that this script has finished loading
 if(typeof eventManager != 'undefined'){
-	eventManager.fire('scriptLoaded', 'vle/node/mysystem/setup.js');
+  eventManager.fire('scriptLoaded', 'vle/node/mysystem_sc/setup.js');
 };
