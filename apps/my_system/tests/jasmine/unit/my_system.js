7304a92c7f8ea0f48669e31a21dff6f0f1e51a8f
Richard Klancer <rpk@pobox.com>
2011-04-13 17:09:45 -0400

    Add example Jasmine spec

---
+++ apps/my_system/tests/jasmine/unit/my_system.js
@@ -0,0 +1,7 @@
+/*globals MySystem describe it expect xit xdescribe beforeEach afterEach spyOn runs waits waitsFor */
+
+describe("MySystem", function () {
+  it("should be defined", function () {
+    expect(MySystem).toBeDefined();
+  });
+});
