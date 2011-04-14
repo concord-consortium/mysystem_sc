6de010c5bcc0c10e8e52f38dc815c011b7c42c5e
Richard Klancer <rpk@pobox.com>
2011-04-14 10:25:51 -0400

    Make the prompt much less silly

--- apps/my_system/fixtures/activity.js
+++ apps/my_system/fixtures/activity.js
@@ -13,7 +13,7 @@ MySystem.Activity.FIXTURES = [
 
   { guid: 'assign1',
     paletteItems: [ 'pi1', 'pi2', 'pi3' ],
-    assignmentText: "<p>Make a MySystem to explain to Gwen how energy from the sun and the things people do BOTH contribute to global climate.</p><ul><li>Where does energy come from?</li><li>How does energy move?</li><li>Where does energy go?</li><li>How does energy change?</li></ul>",
+    assignmentText: "<p>Make a system diagram to explain how energy from the sun and the things people do BOTH contribute to global climate.</p><ul><li>Where does energy come from?</li><li>How does energy move?</li><li>Where does energy go?</li><li>How does energy change?</li></ul>",
     energyTypes: [ 'et1', 'et2', 'et3', 'et4', 'et5' ]
   	}
   
--- apps/my_system/fixtures/story.js
+++ apps/my_system/fixtures/story.js
@@ -13,6 +13,6 @@ MySystem.Story.FIXTURES = [
   //   lastName: "Scott" },
 
 	{ guid: 1,
-		storyHtml: "<p>Make a MySystem to explain to Gwen how energy from the sun and the things people do BOTH contribute to global climate.</p><ul><li>Where does energy come from?</li><li>How does energy move?</li><li>Where does energy go?</li><li>How does energy change?</li></ul>" }
+		storyHtml: "<p>Make a system diagram to explain how energy from the sun and the things people do BOTH contribute to global climate.</p><ul><li>Where does energy come from?</li><li>How does energy move?</li><li>Where does energy go?</li><li>How does energy change?</li></ul>" }
 
 ];
