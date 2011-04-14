3009e294bba9207c14f86fef14e504066955df83
Richard Klancer <rpk@pobox.com>
2011-04-14 11:06:49 -0400

    Un-muddle the prompt.

--- apps/my_system/fixtures/activity.js
+++ apps/my_system/fixtures/activity.js
@@ -10,7 +10,7 @@ MySystem.Activity.FIXTURES = [
 
   { guid: 'assign1',
     paletteItems: [ 'pi1', 'pi2', 'pi3' ],
-    assignmentText: "<p>Make a system diagram to explain how energy from the sun and the things people do BOTH contribute to global climate.</p><ul><li>Where does energy come from?</li><li>How does energy move?</li><li>Where does energy go?</li><li>How does energy change?</li></ul>",
+    assignmentText: "<p>Make a system diagram to help explain how <i>both</i> the sun and people's actions affect the Earth's climate.</p><ul><li>Where does energy come from?</li><li>How does energy move?</li><li>Where does energy go?</li><li>How does energy change?</li></ul>",
     energyTypes: [ 'et1', 'et2', 'et3', 'et4', 'et5' ]
   	}
   
--- apps/my_system/fixtures/story.js
+++ apps/my_system/fixtures/story.js
@@ -9,7 +9,7 @@ sc_require('models/story');
 MySystem.Story.FIXTURES = [
 
 	{ guid: 1,
-		storyHtml: "<p>Make a system diagram to explain how energy from the sun and the things people do BOTH contribute to global climate.</p><ul><li>Where does energy come from?</li><li>How does energy move?</li><li>Where does energy go?</li><li>How does energy change?</li></ul>"
+		storyHtml: "<p>Make a system diagram to help explain how <i>both</i> the sun and people's actions affect the Earth's climate.</p><ul><li>Where does energy come from?</li><li>How does energy move?</li><li>Where does energy go?</li><li>How does energy change?</li></ul>"
 	}
 
 ];
