34f77c71ea2a06ea100386175fa2268718398723
Richard Klancer <rpk@pobox.com>
2011-04-27 12:13:29 -0400

    Clarify the example assignment text.

--- apps/my_system/fixtures/activity.js
+++ apps/my_system/fixtures/activity.js
@@ -10,7 +10,7 @@ MySystem.Activity.FIXTURES = [
 
   { guid: 'assign1',
     paletteItems: [ 'pi1', 'pi2', 'pi3' ],
-    assignmentText: "<p>Make a system diagram to help explain how <i>both</i> the sun and people's actions affect the Earth's climate.</p><ul><li>Where does energy come from?</li><li>How does energy move?</li><li>Where does energy go?</li><li>How does energy change?</li></ul>",
+    assignmentText: "<p>Make a diagram and story to help explain how <i>both</i> the sun and people's actions affect the Earth's climate.</p><ul><li>Where does the energy originally come from?</li><li>How does the energy move through the system?</li><li>How does the energy change as it moves through the system?</li><li>Where does the energy go in the end?</li></ul>",
     energyTypes: [ 'et1', 'et2', 'et3', 'et4', 'et5' ]
   	}
   
