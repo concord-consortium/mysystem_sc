be30f2db872909a4f2cf9c82adbd417f8bb4e6ea
Aaron Unger <aunger@concord.org>
2011-08-09 09:49:09 -0700

    Simplify the radioRenderDelegate so that we're not duplicating a bunch of the core sproutcore code.

--- apps/my_system/render_delegates/radio.js
+++ apps/my_system/render_delegates/radio.js
@@ -31,29 +31,7 @@ MySystem.Theme.radioRenderDelegate = SC.RenderDelegate.create({
   className: 'radio',
   
   render: function(dataSource, context) {
-    this.addSizeClassName(dataSource, context);
-
-    var theme = dataSource.get('theme');
-    
-    var isSelected = dataSource.get('isSelected'),
-        width = dataSource.get('width'),
-        color = dataSource.get('color'),
-        labelId = SC.guidFor(dataSource) + '-label';
-
-    context.setClass({
-      active: dataSource.get('isActive'),
-      mixed: dataSource.get('isMixed'),
-      sel: dataSource.get('isSelected'),
-      disabled: !dataSource.get('isEnabled')
-    });
-
-    //accessing accessibility
-    context.attr('role', 'radio');
-    context.attr('aria-checked', isSelected);
-    context.attr('aria-labelledby', labelId);
-    context.attr('aria-disabled', dataSource.get('isEnabled') ? 'false' : 'true');
-
-    if (width) context.css('width', width);
+    var color = dataSource.get('color');
 
     if (color) {
       context.css('background-color', color);
@@ -61,36 +39,17 @@ MySystem.Theme.radioRenderDelegate = SC.RenderDelegate.create({
     }
 
     context.css('font-weight', 'bold');
-    context.push('<span class = "button"></span>');
 
-    context = context.begin('span').addClass('sc-button-label').id(labelId);
-    theme.labelRenderDelegate.render(dataSource, context);
-    context = context.end();
+    // delegate the rest of the handling to the original
+    // radioRenderDelegate
+    SC.BaseTheme.radioRenderDelegate.render(dataSource, context);
   },
 
   update: function(dataSource, jquery) {
-    this.updateSizeClassName(dataSource, jquery);
-
-    var theme = dataSource.get('theme');
-
-    var isSelected = dataSource.get('isSelected'),
-        width = dataSource.get('width'),
-        color = dataSource.get('color'),
-        value = dataSource.get('value');
-
-    jquery.setClass({
-      active: dataSource.get('isActive'),
-      mixed: dataSource.get('isMixed'),
-      sel: dataSource.get('isSelected'),
-      disabled: !dataSource.get('isEnabled')
-    });
-
-    jquery.attr('aria-disabled', dataSource.get('isEnabled') ? 'false' : 'true');
-    jquery.attr('aria-checked', isSelected);
-    jquery.css('width', width ? width : null);
+    var color = dataSource.get('color');
     jquery.css('background-color', color ? color : null);
     jquery.css('color', color ? "#FFFFFF" : null);
 
-    theme.labelRenderDelegate.update(dataSource, jquery.find('.sc-button-label'));
+    SC.BaseTheme.radioRenderDelegate.update(dataSource, jquery);
   }
 });
