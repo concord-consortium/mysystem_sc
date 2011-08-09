f79adf260e0b09c60b3ec1b3641d5f1d3cf4bcdf
Sam Fentress <sfentress@concord.org>
2011-08-09 12:02:48 -0400

    Method of finding canvas is still brittle, but fix it so it won't cause NPE

--- apps/my_system/statechart.js
+++ apps/my_system/statechart.js
@@ -110,9 +110,12 @@ MySystem.statechart = SC.Object.create(SC.StatechartManager, {
     // Note this will only work for views with a statechart as their default responder. Any
     // view for which acceptsFirstResponder = YES will need to handle this themselves.
     keyDown: function(evt) {
-      var myCanvas = MySystem.getPath('mainPage.mainPane.childViews').objectAt(0).getPath('bottomRightView.bottomRightView');
-      if (!!myCanvas){
-        myCanvas.keyDown(evt);
+      var childViews = MySystem.getPath('mainPage.mainPane.childViews');
+      if (!!childViews && childViews.length > 0){
+        var myCanvas = MySystem.getPath('mainPage.mainPane.childViews').objectAt(0).getPath('bottomRightView.bottomRightView');
+        if (!!myCanvas){
+          myCanvas.keyDown(evt);
+        }
       }
       //swallow key down event
       return YES;
