3eb004eed105f29927250e03dd231d09776fcaba
Sam Fentress <sfentress@concord.org>
2011-08-11 12:13:07 -0400

    Refactor rule checking to be a little cleaner

--- apps/my_system/controllers/activity.js
+++ apps/my_system/controllers/activity.js
@@ -18,5 +18,20 @@
 
 
 MySystem.activityController = SC.ObjectController.create({
+  
+  runDiagramRules: function() {
+    var rules = MySystem.activityController.get('diagramRules'),
+        nodes = MySystem.store.find(MySystem.Node),
+        suggestions = [];
+
+    rules.forEach( function (rule) {
+      if (!rule.check(nodes)) {
+        suggestions.pushObject(rule.get('suggestion'));
+      }
+    });
+    
+    return suggestions;
+  }
+  
 }) ;
 
