7f91b3b7102577b61fef9afe0d5b8fab5030a44a
Richard Klancer <rpk@pobox.com>
2011-08-19 10:41:13 -0400

    migrateLearnerData1

--- apps/my_system/migrations/migrate_learner_data_1.js
+++ apps/my_system/migrations/migrate_learner_data_1.js
@@ -6,7 +6,30 @@
 
 sc_require('migrations/migrations');
 
+/** @scope MySystem.migrations 
+  
+    Migrate learner data from format 1 to format 2  
+*/
 MySystem.migrations.migrateLearnerData1 = function (data) {
 
+  // TODO it would be nice to define these transformations declaratively with a JSONPath type syntax rather than 
+  // imperatively
+  
+  var ret = SC.copy(data, YES), // YES => deep copy
+      dataNodes = data["MySystem.Node"],
+      retNodes = ret["MySystem.Node"];
+  
+  for (var p in dataNodes) {
+    if (!dataNodes.hasOwnProperty(p)) continue;
 
+    delete retNodes[p].position;               // to be replaced by x, y
+    retNodes[p].x = dataNodes[p].position.x;
+    retNodes[p].y = dataNodes[p].position.y;
+    
+    delete retNodes[p].transformer;           // no actual students used this field, don't save it for later
+    delete retNodes[p].transformations;       // no actual students used this field, don't save it for later
+  }
+  
+  ret.version = 2;
+  return ret;
 };
