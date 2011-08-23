b57a3a42e05e88b10cea5dc22e7d90b0379bf4d4
Richard Klancer <rpk@pobox.com>
2011-08-23 13:19:32 -0400

    Remove 'dirOrdering' tests that screw up the individual TestRunner tests

--- apps/my_system/tests/jasmine/integration/dir_ordering.js
+++
@@ -1,9 +0,0 @@
-/*globals describe it TestDirectoryLoaded */
-// we want to make sure the directories are being parsed in the same order on our various test environments
-describe("The integration folder", function () {
-  it("should be loaded first", function() {
-    expect(window.TestDirectoryLoaded).not.toBeDefined();
-    window.TestDirectoryLoaded = 1;
-  }); 
-});
-
