41b067798d25149b01b6a8674f7b2dc995c3b583
Sam Fentress <sfentress@concord.org>
2011-08-24 14:11:51 -0400

    Auto save every 20 seconds while data is dirty

--- apps/my_system/controllers/saving_controller.js
+++ apps/my_system/controllers/saving_controller.js
@@ -10,6 +10,8 @@ MySystem.savingController = SC.Object.create({
   
   dataIsDirty: NO,
   
+  saveTimer: null,
+  
   saveStatusText: function(){
     if(!this.get('saveFunction')){
       return 'No External Saving';
@@ -33,5 +35,35 @@ MySystem.savingController = SC.Object.create({
     if (successful){
       this.set('dataIsDirty', NO);
     }
-  }
+  },
+	
+	// This timer will be scheduled to attempt to save every twenty seconds while
+	// data is dirty. Once data is clean, the timer will be cancelled.
+	//
+	// From the point of view of the user, no data saving will happen until they modify their data,
+	// then after twenty seconds a save will be triggered. Assuming the save was successful,
+	// no more saving will happen until they modify their data again. If they are continuously changing
+	// the diagram, the system will save every twenty seconds.
+	scheduleTimer: function() {
+	  if (!this.get('saveFunction')){
+	    return;
+	  }
+	  
+	  if (this.get('dataIsDirty')) {
+	    if (!this.get('saveTimer')){  // if we already have a timer, don't make a new one
+	      // save ten seconds after data was first made dirty
+	      this.set('saveTimer', SC.Timer.schedule({
+          target: this,
+          action: 'save',
+          interval: 20000,
+          repeats: YES
+        }));
+	    }
+	  } else {
+	    if (!!this.get('saveTimer')){
+	      this.get('saveTimer').invalidate();
+	      this.set('saveTimer', null);
+      }
+	  }
+	}.observes('dataIsDirty')
 });
