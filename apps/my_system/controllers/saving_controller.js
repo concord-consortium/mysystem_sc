cc2387480aff72becf51f60f0bd10130c8486d04
Noah Paessel <knowuh@gmail.com>
2011-08-25 18:03:05 -0400

    cleanup display timer.

--- apps/my_system/controllers/saving_controller.js
+++ apps/my_system/controllers/saving_controller.js
@@ -6,43 +6,33 @@
 /*globals MySystem */
 MySystem.savingController = SC.Object.create({
   // this should be set 
-  saveFunction: null,
-  saveTime: null,
-  saveStatusText: 'not yet saved',
-  autoSaveFrequency: 20000,
+  saveFunction:      null,
+  saveTime:          null,
+  displayTime:       null,
+  saveTimer:         null,
+  autoSaveFrequency: 20000, // save every          20 seconds
+  displayFrequency:   5000, // update disaply every 5 seconds 
 
-  displayTimer: SC.Timer.schedule({ 
-            target:   'MySystem.savingController',
-            action:   'updateStatusText',
-            interval: 5000,
-            repeats:  YES }),
-
-  updateStatusText: function() {
-    var timeNow  = new Date().getTime(),
-        saveTime = this.get('saveTime'),
+  saveStatusText: function() {
+    var saveTime = this.get('saveTime'),
         seconds  = 0,
         minutes  = 0,
-        hours    = 0;
-    if (!!!saveTime) { return; }
+        hours    = 0,
+        timeNow  = new Date().getTime();
+    
+    if (!!!saveTime) { return 'not yet saved'; }
+
     seconds = (timeNow - saveTime) / 1000.0;
     minutes = seconds / 60;
     hours   = minutes / 60;
 
-    if (seconds < 10) {
-      this.set('saveStatusText', 'Saved: just now');
-      return;
-    }
-    if (seconds < 60) {
-      this.set('saveStatusText', 'Saved: ' + Math.round(seconds) + ' seconds ago');
-      return;
-    }
-    if (minutes < 60) {
-      this.set('saveStatusText', 'Saved: ' + Math.round(minutes) + ' minutes ago');
-      return;
-    }
-    this.set('saveStatusText',   'Saved: ' + Math.round(hours) +   ' hours ago');
-  },
-  
+    if (seconds < 10) { return ('Saved: just now'); }
+    if (seconds < 60) { return ('Saved: ' + Math.round(seconds) + ' seconds ago'); }
+    if (minutes < 60) { return ('Saved: ' + Math.round(minutes) + ' minutes ago'); }
+    return ('Saved: '                     + Math.round(hours)   + ' hours ago');
+
+  }.property('saveTime', 'displayTime'),
+
   enableManualSave: function(){
     return !!this.get('saveFunction') && !!this.get('dataIsDirty');
   }.property('saveFunction', 'dataIsDirty'),
@@ -50,6 +40,7 @@ MySystem.savingController = SC.Object.create({
   save: function() {
     if(this.get('saveFunction')) {
       this.get('saveFunction')();
+      this.set('saveTime', new Date().getTime());
     }
   },
   
@@ -70,10 +61,8 @@ MySystem.savingController = SC.Object.create({
 	// then after twenty seconds a save will be triggered. Assuming the save was successful,
 	// no more saving will happen until they modify their data again. If they are continuously changing
 	// the diagram, the system will save every twenty seconds.
-	scheduleTimer: function() {
-	  if (!this.get('saveFunction')){
-	    return;
-	  }
+	scheduleAutoSave: function() {
+	  if (!this.get('saveFunction')){  return; }
 
 	  if (this.get('dataIsDirty') && this.get('autoSaveFrequency') > 0) {
 	    if (!this.get('saveTimer')){  // if we already have a timer, don't make a new one
@@ -91,11 +80,35 @@ MySystem.savingController = SC.Object.create({
 	      this.set('saveTimer', null);
       }
 	  }
-	}.observes('dataIsDirty', 'autoSaveFrequency')
-});  },
+	}.observes('dataIsDirty', 'autoSaveFrequency'),
+  
+
+  updateDisplayTime: function() {
+    this.set('displayTime', new Date().getTime());
+  },
 
   saveSuccess: function() {
     var time = new Date().getTime();
     this.set('saveTime',time);
+  },
+
+  init: function() {
+    sc_super();
+    var timer = SC.Timer.schedule({
+                  target:   this,
+                  action:   'updateDisplayTime',
+                  interval: this.get('displayFrequency'),
+                  repeats:  YES
+                });
+
+    this.set('displayTimer', timer);
+  },
+
+  destroy: function() {
+    this.get('saveTimer').invalidate();
+    this.set('saveTimer', null);
+    this.get('displayTimer').invalidate();
+    this.set('displayTimer', null);
+    sc_super();
   }
 });
