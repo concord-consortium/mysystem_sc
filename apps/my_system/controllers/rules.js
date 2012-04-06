c6ecfcf1c57e4a4de91ecde2e77bd16f1c3cf2a9
Noah Paessel <knowuh@gmail.com>
2012-04-06 12:38:42 -0400

    Custom rule stack trace. Code cleanup, refactor.

--- apps/my_system/controllers/rules.js
+++ apps/my_system/controllers/rules.js
@@ -5,31 +5,29 @@
 
 /*globals MySystem */
 
-
 /**
  @class MySystem.rulController 
   Description: orchestrates rule evaluation, with rules, nodes, and custom expressions.
   @extends SC.ObjectController
 */
 MySystem.rulesController = SC.ObjectController.create({
-  success:  false,
-  feedback: "(empty feedback)",
 
-  // runs the diagram rules, saves the results to the learner data and returns
-  // an array containing a success boolean and a feedback string
-  runDiagramRules: function() {
-    var rules                 = MySystem.activityController.get('diagramRules'),
-    minimumRequirements       = MySystem.activityController.get('minimumRequirements'),
-    minimumRequirementsFB     = MySystem.activityController.get('minimumRequirementsFeedback'),
-    customRuleEvaluator       = MySystem.activityController.get('customRuleEvaluator'),
-    enableCustomRuleEvaluator = MySystem.activityController.get('enableCustomRuleEvaluator'),
-    correctFeedback           = MySystem.activityController.get('correctFeedback'),
-    nodes                     = MySystem.store.find(MySystem.Node),
-    success                   = this.get('success'),
-    feedback                  = this.get('feedback'),
-    suggestions               = [];
+  rulesBinding: SC.Binding.oneWay('MySystem.activityController.diagramRules'),
+  success     : false,
+  feedback    : "(empty feedback)",
+  suggestions : [],
+  nodes       : [],
 
-    var reqFails = 0;
+  addSuggestion: function(suggestion) {
+    this.suggestions.push(suggestion);
+  },
+
+  checkMinimumFeedback: function() {
+    var reqFails          = 0,
+    nodes                 = this.nodes;
+    minimumRequirements   = MySystem.activityController.get('minimumRequirements'),
+    minimumRequirementsFB = MySystem.activityController.get('minimumRequirementsFeedback');
+
     minimumRequirements.forEach( function(rule) {
       if (!rule.check(nodes)) {
         reqFails++;
@@ -38,53 +36,114 @@ MySystem.rulesController = SC.ObjectController.create({
 
     // abort if any of the minimum requirement rules fails
     if (reqFails > 0) {
-      suggestions.pushObject(minimumRequirementsFB);
-      return suggestions;
+      this.addSuggestion(minimumRequirementsFB);
+      return false;
+    }
+    return true;
+  },
+
+  findRule: function(name) {
+    var rules = this.get('rules'),
+    rule      = null,
+    i         = 0;
+
+    for(i=0; i< rules.length; i++) {
+      rule = rules[i];
+      if (rule.get('name') == name) return rule;
+    }
+    return null;
+  },
+
+  // run one rule:
+  runRule: function(rule) {
+    if (typeof rule == "string") {
+      rule = this.findRule(rule);
     }
 
-    // allow custom JS eval here in this context.
-    if (enableCustomRuleEvaluator) {
-      try {
-        // TODO: Sanatize or sandbox this!
-        eval(customRuleEvaluator);
-      }
-      catch(e) {
-        if (console && typeof console.log == 'function') {
-          console.log("Error evaluating custom rule:");
-          console.log(e);
-          console.log(customRuleEvaluator);
-          // TODO: Should the end user see this error?
-          // For now, maybe yeah, we fail early.
-          suggestions.pushObject("The custom rule evaluator for this activity is causing an error.");
-          suggestions.pushObject("Check the console for more information.");
-        }
-        else {
-          alert('Error evaluating custom rule, please use firebug and have a look at the console.');
-        }
-      }
+    var nodes = this.nodes;
+    if (!rule.check(nodes)) {
+      this.addSuggestion(rule.get('suggestion'));
     }
+  },
 
-    else {
-      rules.forEach( function (rule) {
-        if (!rule.check(nodes)) {
-          suggestions.pushObject(rule.get('suggestion'));
-        }
-      });
+  runAllRules: function() {
+    var rules = MySystem.activityController.get('diagramRules'),
+    self      = this;
+
+    rules.forEach( function (rule) {
+      self.runRule(rule);
+    });
+  },
+
+  // runDiagramRules: runs the diagram rules
+  // updates "success" and "feedback" properties
+  runDiagramRules: function() {
+    var rules                 = this.get('rules'),
+    enableCustomRuleEvaluator = MySystem.activityController.get('enableCustomRuleEvaluator'),
+    correctFeedback           = MySystem.activityController.get('correctFeedback'),
+    success                   = this.get('success'),
+    feedback                  = this.get('feedback');
+
+    // clear out previous data:
+    var suggestions = this.suggestions = [];
+
+    // it would be nice if there was a nodes(only) controller:
+    var nodes = this.nodes = MySystem.store.find(MySystem.Node);
+    
+    if (this.checkMinimumFeedback()) {
+      if (enableCustomRuleEvaluator) {
+        this._evaluateCustomRules(this.suggestions);
+      }
+      else {
+        this.runAllRules();
+      }
     }
 
     this._trimFeedback(suggestions);
 
-    success  = (suggestions.get('length') === 0);
+    success  = (this.suggestions.get('length') === 0);
     feedback = success ? correctFeedback : suggestions.join(" \n");
+
     this.set('success', success);
     this.set('feedback', feedback);
   },
+
 
   _trimFeedback: function(suggestions){
     var maxFeedback = MySystem.activityController.get('maxFeedbackItems');
     if (maxFeedback && maxFeedback > 0 && suggestions.length > maxFeedback) {
       suggestions = suggestions.slice(0,maxFeedback);
     }
+  },
+
+  _evaluateCustomRules: function() {
+      // provide context to the evaluator:
+      var customRuleEvaluator       = MySystem.activityController.get('customRuleEvaluator');
+      var nodes                     = MySystem.store.find(MySystem.Node);
+      var correctFeedback           = MySystem.activityController.get('correctFeedback');
+      var suggestions               = this.suggestions;
+
+      var lines = customRuleEvaluator.split(/\r\n|\r|\n/);
+      current_line = "";
+      counter   = 0;
+      try {
+        for (counter=0; counter < lines.length; counter++) {
+          current_line = lines[counter];
+          eval(current_line);
+        }
+      }
+      catch(e) {
+        if (console && typeof console.log == 'function') {
+          console.log("Error evaluating custom rule, line:" + counter + 1);
+          console.log("'" + current_line + "'");
+          console.log(e);
+          console.log(customRuleEvaluator);  // so that it can be inspected.
+        }
+        // WARNING:  Errors will be dispalyed to users:
+        suggestions.pushObject("Rule Evaluation Error: line " + (counter + 1));
+        suggestions.pushObject("'" + current_line + "'");
+        suggestions.pushObject("Check the console for more information.");      
+      }
   }
 
 });
