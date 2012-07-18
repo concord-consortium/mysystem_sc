c5a892449076dc4eab8e18333bddf4b3695383e6
Noah Paessel <knowuh@gmail.com>
2012-07-18 17:43:23 -0400

    Don't 'load' blank images. (prevent JS error message from appearing).

--- apps/my_system/views/diagram_background.js
+++ apps/my_system/views/diagram_background.js
@@ -62,8 +62,15 @@ MySystem.DiagramBackgroundView = RaphaelViews.RaphaelView.extend(SC.ContentDispl
       'width': this.get('width'),
       'height': this.get('height')
     };
-    
-    attrs.src = this.get('imageUrl');
+
+    // dont load 'blank' images.
+    var imageUrl = this.get('imageUrl');
+    if (typeof imageUrl === 'string') {
+      var nowhite = imageUrl.replace(/\s+/,"");
+      if (nowhite.length > 0) {
+        attrs.src = imageUrl;
+      }
+    }
     
     if (firstTime) {
       this._reloadImage();
